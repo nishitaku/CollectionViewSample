1528ca42fa90663d513178280b920cb82cae2297
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-07-03 15:08:28 +0900

    first commit

---
+++ CollectionViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by SRA01 on 2020/07/02.
+//  Copyright © 2020 SRA01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
